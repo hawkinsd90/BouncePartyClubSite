d142fdd098521acf950eb861c78da7048ec9e528
SlimKilla <Hawkinsd90@gmail.com>
2026-03-20 00:48:33 -0400

    Updated CustomerPortal.tsx

--- src/pages/CustomerPortal.tsx
+++ src/pages/CustomerPortal.tsx
@@ -63,6 +63,7 @@ export function CustomerPortal() {
             return;
           }
 
+          await loadOrder(orderId, invoiceToken ?? undefined, isInvoiceLink);
           setInvoiceProcessing(false);
           setApprovalSuccess(true);
         } catch (err: any) {
